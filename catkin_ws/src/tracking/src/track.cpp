ca3ffbece9c37c5ba8fee63ec88c5fbd49084d14
allenyh <bicycleboy323@gmail.com>
2019-06-16 16:08:03 +0800

    add fiter condition and publish id and cluster pointcloud

--- catkin_ws/src/tracking/src/track.cpp
+++ catkin_ws/src/tracking/src/track.cpp
@@ -9,10 +9,9 @@ Track::Track(ros::NodeHandle nh, ros::NodeHandle pnh): nh_(nh), pnh_(pnh){
   cloud_last_frame = PointCloudXYZIPtr (new PointCloudXYZI ());
   // Subscriber and Publisher
   lidar_sub = nh_.subscribe("/points_raw", 1, &Track::cb_lidar, this);
-  result_pub = pnh_.advertise<visualization_msgs::MarkerArray>("text", 1);
-  filtered_pub = pnh_.advertise<sensor_msgs::PointCloud2>("filtered", 1);
   cluster_pub = pnh_.advertise<sensor_msgs::PointCloud2>("cluster", 1);
   bb_pub = pnh_.advertise<visualization_msgs::MarkerArray>("box", 1);
+  id_pub = pnh_.advertise<visualization_msgs::MarkerArray>("id", 1);
   // ICP converge criteria
   icp.setMaximumIterations(100);
   icp.setTransformationEpsilon(1e-3);
@@ -47,15 +46,15 @@ void Track::process_data(void){
     Time time_source = get<0>(tuple_source);
     CentroidVector centV_source = get<1>(tuple_source);
     ClusterVector clusV_source = get<2>(tuple_source);
+    ResultVector rv;
     // First Process
     if(firstProcess){
-      ResultVector rv_first;
       firstProcess = false;
       for(int count=0; count<centV_source.size(); ++count){
         ResultTuple rt = make_tuple(time_source, count, centV_source.at(count), clusV_source.at(count));
-        rv_first.push_back(rt);
+        rv.push_back(rt);
       }
-      RVector.push_back(rv_first);
+      RVector.push_back(rv);
     }
     // Rest Process
     else if(TupleVector.size() > 1){
@@ -65,18 +64,17 @@ void Track::process_data(void){
       Time time_target = get<0>(tuple_target);
       CentroidVector centV_target = get<1>(tuple_target);
       ClusterVector clusV_target = get<2>(tuple_target);
-      PointCloudXYZIPtr cloud_source = get<3>(tuple_source);
-      PointCloudXYZIPtr cloud_target = get<3>(tuple_target);
+      PointCloudXYZI cloud_source = get<3>(tuple_source);
+      PointCloudXYZI cloud_target = get<3>(tuple_target);
       // Find tf between two pointcloud
       ros::Time icp_t = ros::Time::now();
       PointCloudXYZI final_cloud;
-      icp.setInputSource(cloud_source);
-      icp.setInputTarget(cloud_target);
+      icp.setInputSource(cloud_source.makeShared());
+      icp.setInputTarget(cloud_target.makeShared());
       icp.align(final_cloud, tf);
       tf = icp.getFinalTransformation();
       cout << "ICP spent time: " << ros::Time::now() - icp_t << endl;
       // Find centroid pair 
-      ResultVector rv;
       int no_match_count = 0;
       ros::Time t = ros::Time::now();
       for(int target_count=0; target_count < centV_target.size(); ++target_count){
@@ -110,7 +108,8 @@ void Track::process_data(void){
     else if(bagHasEnd){
       ROS_INFO("All Data Are Processed");
       break;
-    } 
+    }
+    drawBoundingBox(rv); 
   }
 }
 
@@ -144,9 +143,6 @@ void Track::cb_lidar(const sensor_msgs::PointCloud2ConstPtr &lidarMsg){
   ExtractIndices<PointXYZI> extract;
   extract.setInputCloud(cloud_filtered);
   extract.setIndices(inliers);
-  extract.setNegative(false);
-  extract.filter(*cloud_inliers);
-  // Remove inliers
   PointCloudXYZIPtr cloud_remove_inliers(new PointCloudXYZI);
   extract.setNegative(true);
   extract.filter(*cloud_remove_inliers); 
@@ -197,32 +193,38 @@ void Track::cb_lidar(const sensor_msgs::PointCloud2ConstPtr &lidarMsg){
     Vector4f centroid;
     compute3DCentroid(*cloud_cluster, centroid);
     double dist = sqrt(pow(centroid(0), 2) + pow(centroid(1), 2) + pow(centroid(2), 2));
-    if(dist > clusterDistThres) continue; // Too far away, neglect
+    if(dist > clusterDistThres or dist < 2 or centroid(2) > 0.2) continue; // Too far away, neglect
     clus_vec.push_back(*cloud_cluster);
     cent_vec.push_back(centroid);
     *cloud_clusters += *cloud_cluster; // add cluster cloud
   }
-  DataTuple tuple = make_tuple(lidarMsg->header.stamp, cent_vec, clus_vec, cloud_filtered);
+  DataTuple tuple = make_tuple(lidarMsg->header.stamp, cent_vec, clus_vec, *cloud_filtered);
   TupleVector.push_back(tuple);
 
   Time time_end = Time::now();
   cout << "PointCloud Filtering Process Time " << time_end-time_start << endl;
 }
 
-void Track::drawBoundingBox(vector<PointCloud<PointXYZI>> pcVec){
-  visualization_msgs::MarkerArray boxArray;
-  for(size_t i=0; i<pcVec.size(); ++i){
+void Track::drawBoundingBox(ResultVector rv){
+  visualization_msgs::MarkerArray boxArray, idArray;
+  PointCloudXYZI clusterCloud;
+  for(size_t i=0; i<rv.size(); ++i){
+    ResultTuple rt = rv.at(i);
+    int id = get<1>(rt);
+    Vector4f centroid = get<2>(rt);
+    PointCloudXYZI pc = get<3>(rt);
+    clusterCloud += pc;
     PointXYZI minPoint, maxPoint;
-    getMinMax3D(pcVec[i], minPoint, maxPoint);
-    visualization_msgs::Marker bb;
-    bb.header.frame_id = "velodyne";
-    bb.ns = "box_" + std::to_string((int)i);
-    bb.id = (int)i;
-    bb.type = visualization_msgs::Marker::LINE_STRIP;
-    bb.action = visualization_msgs::Marker::ADD;
-    bb.pose.orientation.w = 1.0f;
-    bb.scale.x = 0.1f;
-    bb.color.r = bb.color.g = bb.color.b = bb.color.a = 1.0f;
+    getMinMax3D(pc, minPoint, maxPoint);
+    visualization_msgs::Marker bbMarker, idMarker;
+    bbMarker.header.frame_id = "velodyne";
+    bbMarker.ns = "box_" + std::to_string((int)i);
+    bbMarker.id = (int)i;
+    bbMarker.type = visualization_msgs::Marker::LINE_STRIP;
+    bbMarker.action = visualization_msgs::Marker::ADD;
+    bbMarker.pose.orientation.w = 1.0f;
+    bbMarker.scale.x = 0.1f;
+    bbMarker.color.r = bbMarker.color.g = bbMarker.color.b = bbMarker.color.a = 1.0f;
     geometry_msgs::Point A, B, C, D, E, F, G, H;
     A.x = minPoint.x, A.y = minPoint.y, A.z = minPoint.z; 
     B.x = maxPoint.x, B.y = minPoint.y, B.z = minPoint.z;
@@ -232,14 +234,35 @@ void Track::drawBoundingBox(vector<PointCloud<PointXYZI>> pcVec){
     F.x = maxPoint.x, F.y = minPoint.y, F.z = maxPoint.z;
     G.x = minPoint.x, G.y = maxPoint.y, G.z = maxPoint.z;
     H.x = maxPoint.x, H.y = maxPoint.y, H.z = maxPoint.z;
-    bb.points.push_back(A); bb.points.push_back(B); bb.points.push_back(E); bb.points.push_back(C); 
-    bb.points.push_back(A); bb.points.push_back(D); bb.points.push_back(F); bb.points.push_back(H); 
-    bb.points.push_back(G); bb.points.push_back(D); bb.points.push_back(F); bb.points.push_back(B); 
-    bb.points.push_back(E); bb.points.push_back(H); bb.points.push_back(G); bb.points.push_back(C); 
-    boxArray.markers.push_back(bb);
+    bbMarker.points.push_back(A); bbMarker.points.push_back(B); bbMarker.points.push_back(E); bbMarker.points.push_back(C); 
+    bbMarker.points.push_back(A); bbMarker.points.push_back(D); bbMarker.points.push_back(F); bbMarker.points.push_back(H); 
+    bbMarker.points.push_back(G); bbMarker.points.push_back(D); bbMarker.points.push_back(F); bbMarker.points.push_back(B); 
+    bbMarker.points.push_back(E); bbMarker.points.push_back(H); bbMarker.points.push_back(G); bbMarker.points.push_back(C); 
+    bbMarker.lifetime = Duration(0.5f);
+    boxArray.markers.push_back(bbMarker);
+
+    idMarker.header.frame_id = "velodyne";
+    idMarker.ns = "id_" + std::to_string((int)i);
+    idMarker.id = (int)i;
+    idMarker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    idMarker.action = visualization_msgs::Marker::ADD;
+    idMarker.pose.position.x = centroid(0);
+    idMarker.pose.position.y = centroid(1);
+    idMarker.pose.position.z = centroid(2);
+    idMarker.pose.orientation.w = 1.0f;
+    idMarker.scale.z = 1.0f;
+    idMarker.color.r = idMarker.color.g = idMarker.color.b = idMarker.color.a = 1.0f;
+    idMarker.lifetime = Duration(1.0f);
+    idMarker.text = to_string(id);
+    idArray.markers.push_back(idMarker);
   }
-  ROS_INFO("Cluster Size: %d", (int)pcVec.size());
-  ROS_INFO("Marker Array Size: %d", (int)boxArray.markers.size());
-  ROS_INFO("Marker Point Size: %d", (int)boxArray.markers[0].points.size());
+  //ROS_INFO("Cluster Size: %d", (int)pcVec.size());
+  //ROS_INFO("Marker Array Size: %d", (int)boxArray.markers.size());
+  //ROS_INFO("Marker Point Size: %d", (int)boxArray.markers[0].points.size());
+  sensor_msgs::PointCloud2 pcMsg;
+  toROSMsg(clusterCloud, pcMsg);
+  pcMsg.header.frame_id = "velodyne";
+  cluster_pub.publish(pcMsg);
   bb_pub.publish(boxArray);
+  id_pub.publish(idArray);
 }
